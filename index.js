7149b8fccf5399e76431ce4c5862da8b1c710c80
KiddoKishore <kiddokishore@gmail.com>
2024-03-11 11:30:50 +0530

    Explain the useage of the function using comment line

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 import fs from 'fs';
 import csv from 'csv-parser';
 
-function processData(result, uniqueValues, apiUserCount, data) {
+const result = [];
+const uniqueValues = new Set();
+const apiUserCount = {};
+let grandTotal = 0
+
+function processData(data) {
     const transaction_id = data.transaction_id;
     const api = data.originalurl;
     const uniqueKey = `${transaction_id}-${api}`;
   
+    // Remove the Duplicate values using transition_id and api Url
     if (!uniqueValues.has(uniqueKey)) {
       uniqueValues.add(uniqueKey);
       result.push(data);
+      // Set the Number of Customer for every api
       if (apiUserCount[api]) {
         apiUserCount[api]++;
     } else {
@@ -17,6 +24,7 @@ function processData(result, uniqueValues, apiUserCount, data) {
     }
 }
 
+// Set the charge amount using the Number of Customers uses the api
 function calculateRate(count, api) {
     if (count < 10000) {
       return api === '/v1/checkLiveness' ? 0.06 : 0.07;
@@ -33,22 +41,22 @@ function calculateRate(count, api) {
     }
   }
 
-const result = [];
-const uniqueValues = new Set();
-const apiUserCount = {};
-let grandTotal = 0
-
-fs.createReadStream('data.csv') // Happy Bank- January Consumption.csv
+fs.createReadStream('Happy Bank- January Consumption.csv') // Happy Bank- January Consumption.csv
     .pipe(csv({}))
     .on('error', (err) => console.log(err))
     .on('data', (data) => {
-        processData(result, uniqueValues, apiUserCount, data);
+        processData(data);
     })
     .on('end', () => {
+      // Loop the Customers Count of every api
         Object.keys(apiUserCount).forEach(api => {
+        console.log(`Total Number of Customers in ${api}:`,apiUserCount[api])
         const count = apiUserCount[api];
+        // Get the Charge amount of the api
         const rate = calculateRate(count, api);
+        console.log(`Charge Amount of ${api}:`, rate)
+        // Calculate the Number of Customers and Charge amount of the api
         grandTotal += count * rate;
         });
-    console.log(grandTotal)
+    console.log('Total Charge Amount:',grandTotal)
 });
