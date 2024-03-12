d801c77cb8e2bf6e02fc8a544c783e6e57e35cd2
KiddoKishore <kiddokishore@gmail.com>
2024-03-12 17:47:51 +0530

    initial commit

---
+++ helpers/iterate.js
@@ -0,0 +1,32 @@
+import calculateRate from "./calucate.js";
+
+const IterateData = (apiUserCount,finalResult,api) => {
+    // Iterate through the apiUserCount object for client Id
+    Object.keys(apiUserCount).forEach((app) => {
+        console.log("For Client ID", app);
+        if (!finalResult.app) {
+          finalResult[app] = {};
+        }
+        let grandTotal = 0;
+        // Iterate through the Api Name object for api url
+        Object.keys(apiUserCount[app]).forEach((apiName) => {
+          console.log(
+            `Total Number of Customers in ${apiName}:`,
+            apiUserCount[app][apiName]
+          );
+          const count = apiUserCount[app][apiName];
+  
+          // Call the calculateRate function to calculate the number of customers and charge amount
+          const rate = calculateRate(count, apiName, api);
+          finalResult[app][apiName] = {
+            numberOfCustomers: apiUserCount[app][apiName],
+            total: rate,
+          };
+          grandTotal += rate;
+        });
+        finalResult[app]["totalPrice"] = grandTotal;
+        console.log("Total Charge Amount:", grandTotal);
+      });
+}
+
+export default IterateData
