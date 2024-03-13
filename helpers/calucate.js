e5750931c02bf6b527ac5e9510043a528a89af93
KiddoKishore <kiddokishore@gmail.com>
2024-03-13 13:18:45 +0530

    use the try catch and get the api name

--- helpers/calucate.js
+++ helpers/calucate.js
@@ -1,8 +1,10 @@
 const calculateRate = (count, apiName, api) => {
-  const apiData = api[apiName];
+  const apiData = api['api'][apiName];
   let numberOfCustomer = count;
   let totalPrice = 0;
-  // Iterate through the apiData object for get the charge amount
+
+  try {
+    // Iterate through the apiData object for get the charge amount
   Object.keys(apiData).forEach((api) => {
     const NumberOfCount = parseInt(api); // Make this Object Key String into Number
 
@@ -16,6 +18,9 @@ const calculateRate = (count, apiName, api) => {
     // console.log(totalPrice)
   });
   return totalPrice;
+  } catch (error) {
+    throw new Error(error.message)
+  }
 };
 
 export default calculateRate;
