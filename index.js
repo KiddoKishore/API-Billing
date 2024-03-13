01fba3d1c574a4f6b0bb1347da16fb5131517622
KiddoKishore <kiddokishore@gmail.com>
2024-03-13 13:15:13 +0530

    Create a Api for upload a csv and bill input

--- index.js
+++ index.js
@@ -1,39 +1,22 @@
-import fs from "fs";
-import csv from "csv-parser";
-import processData from "./helpers/data.js";
-import IterateData from "./helpers/iterate.js";
+import express from "express";
+import uploadRoute from './routes/uploadRoute.js';
+import billingRoute from './routes/billingRoute.js';
 
-const apiUserCount = {};
-const client = [];
-const removeDuplicate = new Set();
-const finalResult = [];
+const app = express();
+const port = 5000;
 
-const api = {
-  "/v1/checkLiveness": {
-    10000: 0.06,
-    30000: 0.05,
-    100000: 0.04,
-    300000: 0.03,
-    500000: 0.02,
-    1000000: 0.01,
-  },
-  "/v1/matchFace": {
-    10000: 0.07,
-    30000: 0.06,
-    100000: 0.05,
-    300000: 0.04,
-    500000: 0.03,
-    1000000: 0.03,
-  },
-};
+app.use(express.json());
 
-fs.createReadStream("Happy Bank- January Consumption.csv") // Happy Bank- January Consumption.csv
-  .pipe(csv({}))
-  .on("error", (err) => console.log(err))
-  .on("data", (data) => {
-    processData(data, client, removeDuplicate, apiUserCount);
-  })
-  .on("end", () => {
-    IterateData(apiUserCount,finalResult,api)
-    console.log(finalResult);
-  });
+app.use('/', uploadRoute)
+
+app.use('/', billingRoute)
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: err.message });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running in Port:${port}`);
+});
