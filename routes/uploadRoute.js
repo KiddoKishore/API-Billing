c6e9ef914ccfd54a20ba1740899d85ea99acb3c6
KiddoKishore <kiddokishore@gmail.com>
2024-03-13 19:31:47 +0530

    initial commit

--- routes/uploadRoute.js
+++ routes/uploadRoute.js
@@ -1,40 +1,9 @@
 import express from 'express';
-import multer from 'multer';
-import path from 'path'
+import { uploadFileMiddleware } from '../middleware/uploadFileMiddleware.js';
+import { uploadController } from '../controllers/uploadController.js';
 
 const router = express.Router();
 
-// Set storage engine
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "uploads/");
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname);
-    },
-  });
-
-  const fileFilter = function (req, file, cb) {
-    const ext = path.extname(file.originalname).toLowerCase();
-    if (ext !== '.csv') {
-        return cb(new Error('Only CSV files are allowed'));
-    }
-    cb(null, true);
-};
-
-// Initialize upload
-const upload = multer({ storage, fileFilter });
-
-router.post('/uploadFile', upload.single('file'), (req, res) => {
-    try {
-        res.send({
-            message: 'File Uploaded',
-            file: `${req.file.path}`
-        })
-    } catch (error) {
-        console.log(error)
-        res.send(error)
-    }
-})
+router.post('/uploadFile', uploadFileMiddleware ,uploadController )
 
 export default router;
