d2a031e5e11d50e4ac0b6821f151f751a4bdb706
KiddoKishore <kiddokishore@gmail.com>
2024-03-13 13:20:15 +0530

    Check the status code

--- helpers/data.js
+++ helpers/data.js
@@ -1,16 +1,16 @@
-const processData = (data, client, removeDuplicate, apiUserCount) => {
+const processData = (data, client, removeDuplicate, apiUserCount,api) => {
   const appId = data.appid;
   const transaction_id = data.transaction_id;
-  const api = data.originalurl;
+  const apiUrl = data.originalurl;
 
-  // Charging 200 and 422 response only
-  if (data.statuscode == 200 || data.statuscode == 422) {
+  // Charge the bill using status code
+  if (api.statusCode.includes(parseInt(data.statuscode))) {
     // Check if the appId exists in the client object
     if (!client[appId]) {
       client[appId] = []; // Initialize array if not exists
     }
     // Create a unique key using appId, transaction_id, and api
-    const uniqueKey = `${appId}-${transaction_id}-${api}`;
+    const uniqueKey = `${appId}-${transaction_id}-${apiUrl}`;
 
     // Remove the Duplicate values using transition_id, api Url and App id
     if (!removeDuplicate.has(uniqueKey)) {
@@ -21,10 +21,10 @@ const processData = (data, client, removeDuplicate, apiUserCount) => {
         apiUserCount[appId] = {};
       }
 
-      if (!apiUserCount[appId][api]) {
-        apiUserCount[appId][api] = 1;
+      if (!apiUserCount[appId][apiUrl]) {
+        apiUserCount[appId][apiUrl] = 1;
       } else {
-        apiUserCount[appId][api]++;
+        apiUserCount[appId][apiUrl]++;
       }
     }
   }
