ccd5f2059465b2f174842975f74d44b267ba56e7
KiddoKishore <kiddokishore@gmail.com>
2024-03-13 13:23:20 +0530

    Create a post method for set bill details

---
+++ routes/billingRoute.js
@@ -0,0 +1,53 @@
+import express from 'express';
+import csv from 'csv-parser';
+import processData from "../helpers/data.js";
+import IterateData from "../helpers/iterate.js";
+import fs from 'fs'
+import { inputValidation } from '../middleware/inputValidation.js';
+
+const router = express.Router();
+
+router.post('/processData',inputValidation ,async (req, res)=> {
+    try {
+        const apiUserCount = {};
+        const client = [];
+        const removeDuplicate = new Set();
+        const finalResult = {};
+        const api = req.body;
+    
+        // Ensure that the file name is provided in the request body
+        if (!req.body.fileName) {
+          return res.status(400).send("File name is missing");
+        }
+    
+        const filePath = req.body.fileName
+    
+        // Check if the file exists
+        if (!fs.existsSync(filePath)) {
+          return res.status(404).send("File not found");
+        }
+    
+        // Wrap the file processing in a promise
+        await new Promise((resolve, reject) => {
+          fs.createReadStream(req.body.fileName)
+            .pipe(csv())
+            .on("error", (err) => reject(err))
+            .on("data", (data) => {
+              processData(data, client, removeDuplicate, apiUserCount, api);
+            })
+            .on("end", () => {
+              IterateData(apiUserCount, finalResult, api);
+              resolve();
+              console.log(finalResult);
+            });
+        });
+    
+        // Send the response after processing is complete
+        res.status(200).send(finalResult);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send(error);
+      }
+})
+
+export default router
